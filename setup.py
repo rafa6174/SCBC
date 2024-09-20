0dfa180a23ea1df10579c7789d3b781d5c7ea953
Clavel11 <141845531+Clavel11@users.noreply.github.com>
2024-09-20 12:14:21 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+"""Setup for package pydoku"""
+
+from setuptools import setup
+
+setup(name='Pydoku',
+      version='0.1',
+      description='A Python package to solve a sudoku',
+      url='https://github.com/Clavel11/Pydoku',
+      author='Brenda Escorcia, Clavel Nolasco',
+      author_email='es353611@uaeh.edu.mx, no420760@uaeh.edu.mx',
+      packages=['pydoku'],
+      zip_safe=False)
